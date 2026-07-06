10d9b8c5d6b6a2794bd3f2a97b7d5b53b315fb4e
xaconic <jamesivanipis>
2026-07-06 14:34:13 +0800

    add 3rd number

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>James Ivan I. Panugaling, 3rd Year-Block A</Text>
+      <Text>Mobile App Development</Text>
+    </View>
+  );
+}
