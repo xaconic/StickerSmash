31182d9fec087517225e97e9943b7fe7e3249617
xaconic <jamesivanipis>
2026-07-08 14:32:04 +0800

    added navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import { Tabs } from 'expo-router';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#2bff01',
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
